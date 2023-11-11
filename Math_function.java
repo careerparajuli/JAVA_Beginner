f6bfbf24ed844d546c1f16ed0bff99c9670b1317
careerparajuli <93539685+careerparajuli@users.noreply.github.com>
2023-11-10 20:54:15 -0500

    Added absolute maths function

--- Math_function.java
+++ Math_function.java
@@ -5,6 +5,11 @@ public class Math_function {
 		// TODO Auto-generated method stub
 		int a=3, b=3;
 		System.out.println(Math.pow(a, b));
+
+		//Absolute value
+		int number = -24;
+		number = Math.abs(number);
+		System.out.println("The absolute value is number is: "+number);
 	}
 
 }
